718560c179365773fa4d971b57289ad0680d35a1
howiedanger <kyleforsyth31@hotmail.com>
2021-03-30 18:43:26 +0000

    additional example added

--- hello.js
+++ hello.js
@@ -3,3 +3,9 @@ const sayHello = function(name) {
 }
 
 sayHello("Linus");
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('Johnny');
+console.log(greeting);
